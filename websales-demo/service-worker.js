8426a5285290e0a252e88120e477416d8432f0f0
Gokul <gokuldskp@gmail.com>
2020-08-27 21:58:08 +0530

    Fixed gh-pages and added README

--- websales-demo/service-worker.js
+++ websales-demo/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/websales-demo/precache-manifest.0f83f85dbf4fe60e260b717e517af0cb.js"
+  "./precache-manifest.abf421ab7f4a089b096b91aee7ac2923.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/websales-demo/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
