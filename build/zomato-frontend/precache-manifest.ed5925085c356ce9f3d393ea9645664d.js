06fbb1ac914313bf6e7da5a0333fb14bffcef216
Gokul <gokuldskp@gmail.com>
2020-08-27 19:33:51 +0530

    Added zomato-frontend build files

---
+++ build/zomato-frontend/precache-manifest.ed5925085c356ce9f3d393ea9645664d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c901df91a4fafdaa78259930426db6e1",
+    "url": "/zomato-frontend/index.html"
+  },
+  {
+    "revision": "db4962dea224c7fc8982",
+    "url": "/zomato-frontend/static/css/main.c1b1f5e6.chunk.css"
+  },
+  {
+    "revision": "dca5464fa739dedc1429",
+    "url": "/zomato-frontend/static/js/2.4d84bb03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/zomato-frontend/static/js/2.4d84bb03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db4962dea224c7fc8982",
+    "url": "/zomato-frontend/static/js/main.58f806ef.chunk.js"
+  },
+  {
+    "revision": "3e0697bd86455570b18c",
+    "url": "/zomato-frontend/static/js/runtime-main.8e784e5f.js"
+  }
+]);
