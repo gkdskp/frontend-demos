0b214b0715124b41151836440a502783288e20cd
Gokul <gokuldskp@gmail.com>
2020-08-27 19:10:54 +0530

    Added build files

---
+++ websales-demo/precache-manifest.0f83f85dbf4fe60e260b717e517af0cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "51770436f49a4093035c",
+    "url": "/websales-demo/static/css/main.29dbd60d.chunk.css"
+  },
+  {
+    "revision": "51770436f49a4093035c",
+    "url": "/websales-demo/static/js/main.51770436.chunk.js"
+  },
+  {
+    "revision": "9fe893499ed1a060b71a",
+    "url": "/websales-demo/static/js/runtime~main.9fe89349.js"
+  },
+  {
+    "revision": "c6be179095398f17b10d",
+    "url": "/websales-demo/static/js/2.c6be1790.chunk.js"
+  },
+  {
+    "revision": "a21dca00c44e72ac61a11db7287ffd24",
+    "url": "/websales-demo/index.html"
+  }
+];
